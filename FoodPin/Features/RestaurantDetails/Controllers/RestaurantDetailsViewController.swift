724b370c68328da79f3ba73481ed37b01c4210fb
Vlad Stanescu <vladstanescu94@gmail.com>
2021-02-08 15:45:29 +0200

    Remove restaurant details subviews in preparation for redesign

--- FoodPin/Features/RestaurantDetails/Controllers/RestaurantDetailsViewController.swift
+++ FoodPin/Features/RestaurantDetails/Controllers/RestaurantDetailsViewController.swift
@@ -8,33 +8,15 @@
 import UIKit
 
 class RestaurantDetailsViewController: UIViewController {
-    @IBOutlet var restaurantImageView: UIImageView!
-    @IBOutlet var restaurantDescriptionView: UIView!
     
     var restaurant = Restaurant()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         setupNavigation()
-        setupImageView()
-        setupDetailsDescriptionView()
     }
     
     private func setupNavigation() {
         navigationItem.largeTitleDisplayMode = .never
-    }
-    
-    private func setupImageView() {
-        restaurantImageView.image = UIImage(named: restaurant.image)
-    }
-    
-    private func setupDetailsDescriptionView() {
-        guard let detailsView = restaurantDescriptionView as? RestaurantDetailsDescriptionView else {
-            return
-        }
-        
-        detailsView.restaurantName.text = restaurant.name
-        detailsView.restaurantType.text = restaurant.type
-        detailsView.restaurantLocation.text = restaurant.location
     }
 }
