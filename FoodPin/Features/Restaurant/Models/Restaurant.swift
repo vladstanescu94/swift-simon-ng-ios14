bf9d263b2e5b2e1e4f34e1fc3b0f6f58872c8922
Vlad Stanescu <vladstanescu94@gmail.com>
2021-02-08 15:41:13 +0200

    Update 'Restaurant' model

--- FoodPin/Features/Restaurant/Models/Restaurant.swift
+++ FoodPin/Features/Restaurant/Models/Restaurant.swift
@@ -11,30 +11,32 @@ struct Restaurant: Hashable {
     var location: String = ""
     var type: String = ""
     var isVisited: Bool = false
+    var phone: String = ""
+    var description: String = ""
 }
 
 extension Restaurant {
     static var mockedData: [Restaurant] = [
-        Restaurant(name: "Cafe Deadend", image: "cafedeadend", location: "Hong Kong", type: "Coffee & Tea Shop"),
-        Restaurant(name: "Homei", image: "homei", location: "Hong Kong", type: "Cafe"),
+        Restaurant(name: "Cafe Deadend", image: "cafedeadend", location: "Hong Kong", type: "Coffee & Tea Shop", phone: "0300300300", description: "Lorem ipsum"),
+        Restaurant(name: "Homei", image: "homei", location: "Hong Kong", type: "Cafe", phone: "0300300300", description: "Lorem ipsum"),
         Restaurant(name: "Teakha", image: "teakha", location: "Hong Kong", type: "Tea House"),
-        Restaurant(name: "Cafe Loisl", image: "cafeloisl", location: "Hong Kong", type: "Austrian / Causual Drink"),
-        Restaurant(name: "Petite Oyster", image: "petiteoyster", location: "Hong Kong", type: "French"),
-        Restaurant(name: "For Kee Restaurant", image: "forkee", location: "Hong Kong", type: "Bakery"),
-        Restaurant(name: "Po's Atelier", image: "posatelier", location: "Hong Kong", type: "Bakery"),
-        Restaurant(name: "Bourke Street Bakery", image: "bourkestreetbakery", location: "Sydney", type: "Bakery"),
-        Restaurant(name: "Haigh's Chocolate", image: "haigh", location: "Sydney", type: "Chocolate"),
-        Restaurant(name: "Palomino Espresso", image: "palomino", location: "Sydney", type: "Cafe"),
-        Restaurant(name: "Upstate", image: "upstate", location: "New York", type: "American / Seafood"),
-        Restaurant(name: "Traif", image: "traif", location: "New York", type: "American"),
-        Restaurant(name: "Graham Avenue Meats", image: "graham", location: "New York", type: "American"),
-        Restaurant(name: "Waffle & Wolf", image: "waffleandwolf", location: "New York", type: "Breakfast & Brunch"),
-        Restaurant(name: "Five Leaves", image: "fiveleaves", location: "New York", type: "Coffee & Tea"),
-        Restaurant(name: "Cafe Lore", image: "cafelore", location: "New York", type: "Coffee & Tea"),
-        Restaurant(name: "Confessional", image: "confessional", location: "New York", type: "Latin American"),
-        Restaurant(name: "Barrafina", image: "barrafina", location: "London", type: "Spanish"),
-        Restaurant(name: "Donostia", image: "donostia", location: "London", type: "Spanish"),
-        Restaurant(name: "Royal Oak", image: "royaloak", location: "London", type: "British"),
-        Restaurant(name: "CASK Pub and Kitchen", image: "cask", location: "London", type: "Thai")
+        Restaurant(name: "Cafe Loisl", image: "cafeloisl", location: "Hong Kong", type: "Austrian / Causual Drink", phone: "0300300300", description: "Lorem ipsum"),
+        Restaurant(name: "Petite Oyster", image: "petiteoyster", location: "Hong Kong", type: "French", phone: "0300300300", description: "Lorem ipsum"),
+        Restaurant(name: "For Kee Restaurant", image: "forkee", location: "Hong Kong", type: "Bakery", phone: "0300300300", description: "Lorem ipsum"),
+        Restaurant(name: "Po's Atelier", image: "posatelier", location: "Hong Kong", type: "Bakery", phone: "0300300300", description: "Lorem ipsum"),
+        Restaurant(name: "Bourke Street Bakery", image: "bourkestreetbakery", location: "Sydney", type: "Bakery", phone: "0300300300", description: "Lorem ipsum"),
+        Restaurant(name: "Haigh's Chocolate", image: "haigh", location: "Sydney", type: "Chocolate", phone: "0300300300", description: "Lorem ipsum"),
+        Restaurant(name: "Palomino Espresso", image: "palomino", location: "Sydney", type: "Cafe", phone: "0300300300", description: "Lorem ipsum"),
+        Restaurant(name: "Upstate", image: "upstate", location: "New York", type: "American / Seafood", phone: "0300300300", description: "Lorem ipsum"),
+        Restaurant(name: "Traif", image: "traif", location: "New York", type: "American", phone: "0300300300", description: "Lorem ipsum"),
+        Restaurant(name: "Graham Avenue Meats", image: "graham", location: "New York", type: "American", phone: "0300300300", description: "Lorem ipsum"),
+        Restaurant(name: "Waffle & Wolf", image: "waffleandwolf", location: "New York", type: "Breakfast & Brunch", phone: "0300300300", description: "Lorem ipsum"),
+        Restaurant(name: "Five Leaves", image: "fiveleaves", location: "New York", type: "Coffee & Tea", phone: "0300300300", description: "Lorem ipsum"),
+        Restaurant(name: "Cafe Lore", image: "cafelore", location: "New York", type: "Coffee & Tea", phone: "0300300300", description: "Lorem ipsum"),
+        Restaurant(name: "Confessional", image: "confessional", location: "New York", type: "Latin American", phone: "0300300300", description: "Lorem ipsum"),
+        Restaurant(name: "Barrafina", image: "barrafina", location: "London", type: "Spanish", phone: "0300300300", description: "Lorem ipsum"),
+        Restaurant(name: "Donostia", image: "donostia", location: "London", type: "Spanish", phone: "0300300300", description: "Lorem ipsum"),
+        Restaurant(name: "Royal Oak", image: "royaloak", location: "London", type: "British", phone: "0300300300", description: "Lorem ipsum"),
+        Restaurant(name: "CASK Pub and Kitchen", image: "cask", location: "London", type: "Thai", phone: "0300300300", description: "Lorem ipsum")
     ]
 }
