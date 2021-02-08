7019fe78a22a6d3ada20d2d7ad10403ca0ee1b6d
Vlad Stanescu <vladstanescu94@gmail.com>
2021-02-08 14:16:04 +0200

    Add restaurant details view controller

---
+++ FoodPin/Features/RestaurantDetails/Controllers/RestaurantDetailsViewController.swift
@@ -0,0 +1,15 @@
+//
+//  RestaurantDetailsViewController.swift
+//  FoodPin
+//
+//  Created by Vlad Stanescu on 08.02.2021.
+//
+
+import UIKit
+
+class RestaurantDetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
