d046895c670b8a45f1bb11da4ee30c50480d5771
Vlad Stanescu <vladstanescu94@gmail.com>
2021-02-08 16:51:13 +0200

    Add custom font to name and type label

--- FoodPin/Features/RestaurantDetails/Views/RestaurantDetailsHeaderView.swift
+++ FoodPin/Features/RestaurantDetails/Views/RestaurantDetailsHeaderView.swift
@@ -13,9 +13,19 @@ class RestaurantDetailsHeaderView: UIView {
     @IBOutlet var nameLabel: UILabel! {
         didSet {
             nameLabel.numberOfLines = 0
+            
+            if let customFont = UIFont(name: "Nunito-Bold", size: 40.0) {
+                nameLabel.font = UIFontMetrics(forTextStyle: .title1).scaledFont(for: customFont)
+            }
         }
     }
-    @IBOutlet var typeLabel: UILabel!
+    @IBOutlet var typeLabel: UILabel! {
+        didSet {
+            if let customFont = UIFont(name: "Nunito-Bold", size: 20.0) {
+                typeLabel.font = UIFontMetrics(forTextStyle: .headline).scaledFont(for: customFont)
+            }
+        }
+    }
     @IBOutlet var heartButton: UIButton!
 
 }
