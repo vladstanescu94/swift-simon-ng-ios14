e1f36b5cfd3da5e37eafe0984f3c228d42275572
Vlad Stanescu <vladstanescu94@gmail.com>
2021-02-08 14:12:10 +0200

    Remove option menu from table cell

--- FoodPin/Features/Restaurant/Controllers/RestaurantTableViewController+Extensions.swift
+++ FoodPin/Features/Restaurant/Controllers/RestaurantTableViewController+Extensions.swift
@@ -33,58 +33,6 @@ extension RestaurantTableViewController {
     
     // MARK: - UITableViewDelegate
     
-    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        setupOptionsMenu(indexPath: indexPath)
-        
-        tableView.deselectRow(at: indexPath, animated: false)
-    }
-    
-    // MARK: - Options Alert Menu
-    
-    private func setupOptionsMenu(indexPath: IndexPath) {
-        
-        let optionMenu = UIAlertController(title: nil, message: "What do you want to do?", preferredStyle: .actionSheet)
-        
-        optionMenu.addAction(setupCancelAction())
-        optionMenu.addAction(setupReserveAction())
-        optionMenu.addAction(setupFavoriteAction(indexPath: indexPath))
-        
-        if let popoverController = optionMenu.popoverPresentationController {
-            if let cell = tableView.cellForRow(at: indexPath) {
-                popoverController.sourceView = cell
-                popoverController.sourceRect = cell.bounds
-            }
-        }
-        
-        present(optionMenu, animated: true, completion: nil)
-    }
-    
-    private func setupCancelAction() -> UIAlertAction {
-        return UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
-    }
-    
-    private func setupReserveAction() -> UIAlertAction {
-        let reserveActionHandler = { (action: UIAlertAction!) -> Void in
-            let alertMessage = UIAlertController(title: "Not available yet", message: "Sorry, this feature is not available yet. Please retry later", preferredStyle: .alert)
-            alertMessage.addAction(UIAlertAction(title: "Ok", style: .default, handler: nil))
-            self.present(alertMessage, animated: true, completion: nil)
-        }
-        
-        return UIAlertAction(title: "Reserve a table", style: .default, handler: reserveActionHandler)
-    }
-    
-    private func setupFavoriteAction(indexPath: IndexPath) -> UIAlertAction {
-        let favoriteActionTitle = self.restaurants[indexPath.row].isVisited ? "Remove from favorites" : "Mark as favorite"
-        
-        let favoriteAction = UIAlertAction(title: favoriteActionTitle, style: .default, handler: {
-            (action: UIAlertAction!) -> Void in
-            
-            self.markCellAsFavorite(at: indexPath)
-        })
-        
-        return favoriteAction
-    }
-    
     override func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
         guard let restaurant = self.dataSource.itemIdentifier(for: indexPath) else {
             return UISwipeActionsConfiguration()
